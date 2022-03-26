c4f06d1107403585a978bf1136167daf987bc798
SeongJae Park <sj38.park@gmail.com>
2022-03-26 14:18:22 +0100

    Add a script for capturing
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ capture.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+then
+	echo "Usage: $0 <start page> <end page> <output directory>"
+	exit 1
+fi
+
+start_page=$1
+end_page=$2
+out_dir=$3
+
+for ((i = $start_page; i <= $end_page; i++))
+do
+	page=$(printf "%05d\n" $i)
+	percent=$((i * 100 / (end_page - start_page + 1)))
+	echo "capturing $page page ($percent % done)"
+	adb exec-out screencap -p > "$out_dir/$page.png"
+	adb shell input tap 1000 2000
+	sleep 1
+done
