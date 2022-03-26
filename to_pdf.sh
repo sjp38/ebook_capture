267a16c362bb163b237aba7d045bf3ddb2f3392a
SeongJae Park <sj38.park@gmail.com>
2022-03-26 23:01:16 +0100

    to_pdf: Print current status
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- to_pdf.sh
+++ to_pdf.sh
@@ -9,8 +9,12 @@ fi
 images_dir=$1
 output_pdf=$2
 
+echo "trim"
 mogrify -trim "$images_dir"/*
+echo "compress images"
 pngquant "$images_dir"/*
+echo "make one pdf"
 convert "$images_dir"/*fs8.png +repage "orig-$output_pdf"
+echo "reduce size for ebook"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
 	-dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output_pdf" "orig-$output_pdf"
