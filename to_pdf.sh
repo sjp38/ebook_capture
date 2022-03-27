b4d0da845ddf1aa53646e5152641d3a4bf7c2b78
SeongJae Park <sj38.park@gmail.com>
2022-03-27 09:29:40 +0200

    to_pdf: Make output name based on images directory
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- to_pdf.sh
+++ to_pdf.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-	echo "Usage: $0 <images dir> <output pdf file>"
+	echo "Usage: $0 <images dir>"
 	exit 1
 fi
 
 images_dir=$1
-output_pdf=$2
+output_name=$(basename "$images_dir")
+output_pdf="$output_name.pdf"
+orig_output_pdf="$output_name-orig.pdf"
+
+if [ -f "$output_pdf" ]
+then
+	echo "$output_pdf already exist"
+	exit 1
+fi
+
+if [ -f "$orig_output_pdf" ]
+then
+	echo "$orig_output_pdf already exist"
+	exit 1
+fi
 
 echo "trim"
 mogrify -trim "$images_dir"/*
 echo "compress images"
 pngquant "$images_dir"/*
 echo "make one pdf"
-convert "$images_dir"/*fs8.png +repage "orig-$output_pdf"
+convert "$images_dir"/*fs8.png +repage "$orig_output_pdf"
 echo "reduce size for ebook"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
-	-dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output_pdf" "orig-$output_pdf"
+	-dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output_pdf" "$orig_output_pdf"
