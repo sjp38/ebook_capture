6f3f5a51829c27794cf535ac5821323e0c50ebcc
SeongJae Park <sj38.park@gmail.com>
2022-04-15 00:15:14 +0200

    to_pdf: Fix a typo
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- to_pdf.sh
+++ to_pdf.sh
@@ -50,7 +50,7 @@ gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
 	-sOutputFile="$compressed_output_pdf" "$orig_output_pdf"
 
 orig_sz=$(stat --printf "%s" "$orig_output_pdf")
-compressed_sz=$(stat --printf "%s" "%compressed_output_pdf")
+compressed_sz=$(stat --printf "%s" "$compressed_output_pdf")
 if [ $orig_sz -lt $compressed_sz ]
 then
 	mv "$orig_output_pdf" "$output_pdf"
